33ca8de399522e07302ff052e06bb44cf813a7cd
Curiuskatt <Paula.salas3758@gmail.com>
2026-09-07 00:18:35 -0300

    Add .env

--- simular_ataque.sh
+++ simular_ataque.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-TARGET_IP="127.0.0.1" 
-PORT="4566"
+source .env
+
 echo "=================================================="
-echo "🚀 LANZANDO SIMULACIÓN DE ATAQUE DESDE KALI LINUX"
+echo " LANZANDO SIMULACIÓN DE ATAQUE DESDE KALI LINUX"
 echo "=================================================="
 echo -e "\n[+] 1. Enviando petición maliciosa (admin_panel)..."
 curl -i -H "User-Agent: Kali-Attack-Simulation" "http://$TARGET_IP:$PORT/admin_panel"
+
 echo -e "\n[+] 2. Ejecutando escaneo rápido con Nmap..."
 sudo nmap -sS -F $TARGET_IP
+
 echo "=================================================="
-echo "✅ SIMULACIÓN COMPLETADA."
+echo " SIMULACIÓN COMPLETADA."
 echo "=================================================="
